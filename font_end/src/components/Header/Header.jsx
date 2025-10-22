f94b1c0ec58e61db498ed946b729fdb86aa56a27
23521845-dev <23521845@gm.uit.edu.vn>
2025-10-22 23:39:39 +0700

    change

---
+++ font_end/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Find your interesting trip here </h2>
+        <p>
+          Let's explore and select which places that you wanted to enjoy the natural lifestyle. In additionally, give yourself a chance to move out side the bustle and clustle city to dive into the fresh and peacefull atmostphere in rural.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
